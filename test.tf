5d2d053fdad858f65f6bff4cb7eddab31d5d14d4
parvinder0303 <85359235+parvinder0303@users.noreply.github.com>
2025-03-19 21:54:09 -0500

    Update test.tf

--- test.tf
+++ test.tf
@@ -13,13 +13,13 @@ terraform {
 
 
 provider "aws" {
-  region     = "ca-central-1"
+  region     = "eu-north-1"
 }
 
 resource "aws_instance" "myec2" {
-   ami = "ami-0cc3a9edb87c91b53"
-   instance_type = "t2.micro"
-   key_name = "Ansible"
+   ami = "ami-09a9858973b288bdd"
+   instance_type = "t3.medium"
+   key_name = "27Feb-Public"
    tags = {
       Name = "Example"
    }
