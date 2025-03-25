9856c2d638229274bcadf96b24a92130c27a2844
parvinder0303 <85359235+parvinder0303@users.noreply.github.com>
2025-03-25 05:47:06 -0500

    Update test.tf

--- test.tf
+++ test.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "myec2" {
    ami = "ami-09a9858973b288bdd"
    instance_type = "t3.medium"
   subnet_id = "subnet-083c1f591fccde092"
-   key_name = "ssh-SG-EC2-27Feb"
+   key_name = "27Feb-Public"
    tags = {
       Name = "Example"
    }
